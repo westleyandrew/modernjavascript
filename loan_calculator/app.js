b46ecfbcb2520e89c3db74c5987958f53e3736c6
Westley Andrew <westleyandrew@hotmail.com>
2019-10-01 11:37:34 +0100

    loader and user experience

--- loan_calculator/app.js
+++ loan_calculator/app.js
@@ -1,5 +1,15 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
 function calculateResults(e){
@@ -8,7 +18,6 @@ function calculateResults(e){
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
-
   const monthlyPayment = document.getElementById('monthly-payment');
   const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
@@ -25,15 +34,27 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }       
 
 // Show Error
 function showError(error){
+
+  // Show results
+  document.getElementById('results').style.display = 'none';
+
+  //Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
